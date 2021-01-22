62496ae51105a6371687abbf5988fbf501e7b662
Gernot Leitgab <gernot.leitgab@medunigraz.at>
2021-01-22 11:58:42 +0100

    Apply local asset changes.
    Pull all external assets from npmjs.com through yarn.
    Build assets using grunt.

---
+++ Configuration/TypoScript/constants.ts
@@ -0,0 +1,5 @@
+styles.templates {
+    templateRootPath = EXT:mug_ce/Resources/Private/Templates/ContentElements/
+    partialRootPath = EXT:mug_ce/Resources/Private/Partials/ContentElements/
+    layoutRootPath = EXT:mug_ce/Resources/Private/Layouts/ContentElements/
+}
